9033fc34728f92e6eda93e8e3e847cf8a9c4cf94
TrackSync <tracksyncexperience@gmail.com>
2023-06-29 17:16:48 -0500

    modified:   pages/commands.tsx

--- pages/commands.tsx
+++ pages/commands.tsx
@@ -42,7 +42,7 @@ const Commands: React.FC = () => {
             {commands.map((command: Command, index: number) => (
               <m.div
                 key={index}
-                className="bg-zinc-800 rounded shadow p-4"
+                className="bg-[#f72ae611] rounded shadow p-4"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3, delay: index * 0.1 }}
