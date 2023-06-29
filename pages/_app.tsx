7a8de5f4e1b1676e28f74049ac1a06623c64b793
TrackSync <tracksyncexperience@gmail.com>
2023-06-29 17:19:13 -0500

    modified:   pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import type { AppProps } from 'next/app'
   /* eslint-disable-next-line @next/next/no-img-element */
 }
 
+{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
