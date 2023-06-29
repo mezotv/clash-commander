b901b283a2d24325ec89852d05e28d7c62feacbc
TrackSync <tracksyncexperience@gmail.com>
2023-06-29 11:38:29 -0500

    modified:   components/ServerSlider.tsx 	modified:   package-lock.json modified:   package.json

--- components/ServerSlider.tsx
+++ components/ServerSlider.tsx
@@ -1,7 +1,8 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { LazyMotion, domAnimation, m } from "framer-motion";
-import Marquee from "react-marquee";
-
 
 interface Server {
   id: string;
@@ -15,27 +16,57 @@ interface ServerSliderProps {
 }
 
 const ServerSlider: FC<ServerSliderProps> = ({ servers }) => {
+  const sliderRef = useRef<Slider>(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sliderRef.current) {
+        sliderRef.current.slickNext();
+      }
+    }, 2500); // Adjust the interval duration for smoother sliding
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000, // Adjust slide transition speed for smoother motion
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    rtl: true,
+    swipeToSlide: true,
+    lazyLoad: "ondemand",
+    arrows: false,
+    draggable: false,
+    autoplay: true,
+    autoplaySpeed: 2500, // Adjust the autoplay interval for smoother sliding
+  };
+
   return (
-    <div className="relative w-full h-1/2">
-      <Marquee velocity={50} direction="rtl" resetAfterTries={0}>
+    <div className="relative w-full h-1/2 mb-4">
+      <Slider {...settings} ref={sliderRef}>
         {servers.map((server) => (
           <m.div
             key={server.id}
             style={{
               display: "flex",
               alignItems: "center",
-              height: "200px",
-              padding: "1rem",
+              height: "150px",
+              padding: "0.5rem",
+              marginBottom: "1rem",
             }}
             initial={{ opacity: 0, x: "100%" }}
             animate={{ opacity: 1, x: "0%" }}
             transition={{ duration: 0.5 }}
           >
-            <div className="bg-[#f72ae611] rounded-lg shadow-lg mx-4 p-4 flex items-center">
+            <div className="bg-[#f72ae611] rounded-lg shadow-lg mx-2 mb-10 p-4 flex items-center">
               <img
                 src={server.imageUrl}
                 alt={server.name}
-                className="w-16 h-16 rounded-full object-cover mr-4"
+                className="w-14 h-14 rounded-full object-cover mr-3"
               />
               <div>
                 <h3 className="text-lg font-semibold">{server.name}</h3>
@@ -44,7 +75,7 @@ const ServerSlider: FC<ServerSliderProps> = ({ servers }) => {
             </div>
           </m.div>
         ))}
-      </Marquee>
+      </Slider>
     </div>
   );
 };
